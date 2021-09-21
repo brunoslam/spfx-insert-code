91bbb2d640de0f31212f42c83b3c5d6545d9cffb
Bruno Palma Ávila <bruno.palma94@gmail.com>
2021-09-21 16:59:47 -0300

    changes

--- src/webparts/spfxInsertCode/components/SpfxInsertCode.tsx
+++ src/webparts/spfxInsertCode/components/SpfxInsertCode.tsx
@@ -7,10 +7,10 @@ import * as strings from 'SpfxInsertCodeWebPartStrings';
 
 export default class SpfxInsertCode extends React.Component<ISpfxInsertCodeProps, {}> {
   public componentDidMount() {
-    this.updateScripts(this.props);
+    this.insertScripts(this.props);
   }
   
-  private updateScripts(props: ISpfxInsertCodeProps){
+  private insertScripts(props: ISpfxInsertCodeProps){
 
     var htmlCode = props.htmlCode;
     if (htmlCode && htmlCode.indexOf("<script>") > -1) {
@@ -26,22 +26,36 @@ export default class SpfxInsertCode extends React.Component<ISpfxInsertCodeProps
         htmlCode = htmlCode.replace(scriptInsert, "");
 
         s.innerHTML = scriptInsert.replace("<script>", "").replace("</script>", "");
-        document.getElementById("spfx-insert-code-scriptzone").appendChild(s);
+        document.getElementById("spfx-insert-code-scriptZone").appendChild(s);
       }
     }
   }
 
+  private cleanScriptFromHtml(htmlCode){
+    if (htmlCode && htmlCode.indexOf("<script>") > -1) {
+      while (htmlCode.indexOf("<script>") > -1) {
+        var inicio = htmlCode.indexOf("<script>");
+        var final = htmlCode.indexOf("</script>");
+        var scriptInsert = htmlCode.substring(inicio, final + ("</script>").length);
+        htmlCode = htmlCode.replace(scriptInsert, "");
+      }
+    }
+
+    return htmlCode;
+  }
+
   public componentDidUpdate(prevProps : ISpfxInsertCodeProps){
     // prevProps.htmlCode;
-    this.updateScripts(prevProps);
+    this.insertScripts(prevProps);
   }
 
   public render(): React.ReactElement<ISpfxInsertCodeProps> {
 
     return (
       <div className={styles.spfxInsertCode}>
-        <div id="spfx-insert-code-scriptzone"></div>
-        <div dangerouslySetInnerHTML={{ __html: this.props.htmlCode }}></div>
+        <div id="spfx-insert-code-scriptZone"></div>
+        <div id="spfx-insert-code-htmlZone"></div>
+        <div dangerouslySetInnerHTML={{ __html: this.cleanScriptFromHtml(this.props.htmlCode) }}></div>
       </div>
     );
   }
